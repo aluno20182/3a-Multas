20531cc8b07a71afcd89f412702f6a56f20755d9
Ricardo Pinheiro <47854257+aluno20182@users.noreply.github.com>
2019-04-02 17:01:17 +0100

    v1 - BD e Tabelas Criadas

---
+++ Multas/Multas/Models/Multas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Multas
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public decimal ValorMulta { get; set; }
+
+        public string Descricao { get; set; }
+
+
+    }
+}
---
+++ Multas/Multas/Models/Multas_DB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Multas_DB : DbContext
+    {
+        //identificar qual o SGBD a usar
+
+        //Definir as tabelas da BD
+        public DbSet<Condutores> Condutores { get; set; }
+
+        public DbSet<Viaturas> Viaturas { get; set; }
+
+        public DbSet<Agentes> Agentes { get; set; }
+
+        public DbSet<Multas> Multas{ get; set; }
+
+
+
+    }
+}
