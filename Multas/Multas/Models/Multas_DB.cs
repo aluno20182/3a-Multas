97719debf16bb6918e54ae75fb4383826de10510
Ricardo Pinheiro <47854257+aluno20182@users.noreply.github.com>
2019-04-09 14:43:32 +0100

    Alteração da definição das clases do Modelo_BD

--- Multas/Multas/Models/Multas_DB.cs
+++ Multas/Multas/Models/Multas_DB.cs
@@ -8,18 +8,16 @@ namespace Multas.Models
 {
     public class Multas_DB : DbContext
     {
-        //identificar qual o SGBD a usar
 
-        //Definir as tabelas da BD
-        public DbSet<Condutores> Condutores { get; set; }
+        // MultasDB
 
-        public DbSet<Viaturas> Viaturas { get; set; }
+        public Multas_DB() : base("MultasDBConnectionString") { }
 
-        public DbSet<Agentes> Agentes { get; set; }
-
-        public DbSet<Multas> Multas{ get; set; }
-
-
-
+        // vamos colocar, aqui, as instruções relativas às tabelas do 'negócio'
+        // descrever os nomes das tabelas na Base de Dados
+        public virtual DbSet<Multas> Multas { get; set; } // tabela Multas
+        public virtual DbSet<Condutores> Condutores { get; set; } // tabela Condutores
+        public virtual DbSet<Agentes> Agentes { get; set; } // tabela Agentes
+        public virtual DbSet<Viaturas> Viaturas { get; set; } // tabela Viaturas
     }
 }
